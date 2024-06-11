9971e7ba5d4d39090282c4758d494e0bfc1d0072
devromarc <rmrcslvdr@gmail.com>
2024-06-12 04:01:35 +0800

    Initial Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 1,
+    neutral: 2,
+    bad: 3,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  handleOnClick = kind => {
+    console.log(kind);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = ['good', 'neutral', 'bad'];
+    return (
+      <>
+        <div>
+          {options.map(option => (
+            <button
+              key={option}
+              onClick={() => {
+                this.handleOnClick(option);
+              }}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+        <div>
+          <p>
+            Good <span>{good}</span>
+          </p>
+          <p>
+            Neutral <span>{neutral}</span>
+          </p>
+          <p>
+            Bad <span>{bad}</span>
+          </p>
+          <p>Total:{this.countTotalFeedback()}</p>
+        </div>
+      </>
+    );
+  }
+}
+
+// export const App = () => {
+//
+// };
